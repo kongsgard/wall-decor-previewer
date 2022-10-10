1c425c93d3f0cb4664b4c1bc904d56266ae49598
Sondre Kongsgård <sondre.kongsgaard@gmail.com>
2022-10-10 12:23:34 +0200

    Add model viewer (#4)

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,5 +1,6 @@
 import { Flex, Box } from "@chakra-ui/react";
 import Header from "./Header";
+import { ModelViewer } from "./ModelViewer";
 
 interface Props {
   children: React.ReactNode;
@@ -9,6 +10,7 @@ const Layout: React.FC<Props> = (props: Props) => {
   return (
     <Box minHeight="100vh">
       <Header />
+      <ModelViewer />
       <Flex width="100%">{props.children}</Flex>{" "}
     </Box>
   );
